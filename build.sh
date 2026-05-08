b75158303bb8eb56ec3e168e9d20eac8a742c1d7
logmover <logmover.agent8@gmail.com>
2026-05-08 16:19:19 -0500

    align build.sh to hugo v0.146.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install Hugo
 echo "Installing Hugo..."
-wget -q https://github.com/gohugoio/hugo/releases/download/v0.140.3/hugo_extended_0.140.3_linux-amd64.tar.gz
+wget -q https://github.com/gohugoio/hugo/releases/download/v0.146.0/hugo_extended_0.146.0_linux-amd64.tar.gz
 tar -xzf hugo_extended_0.140.3_linux-amd64.tar.gz
 chmod +x hugo
 ./hugo version
