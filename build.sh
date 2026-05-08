7a33456f9bdaaf8c470a799a04b522a4dd46ee4a
logmover <logmover.agent8@gmail.com>
2026-05-08 16:34:20 -0500

    fix: use correct hugo filename in tar extraction

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Install Hugo
 echo "Installing Hugo..."
 wget -q https://github.com/gohugoio/hugo/releases/download/v0.146.0/hugo_extended_0.146.0_linux-amd64.tar.gz
-tar -xzf hugo_extended_0.140.3_linux-amd64.tar.gz
+tar -xzf hugo_extended_0.146.0_linux-amd64.tar.gz
 chmod +x hugo
 ./hugo version
 
